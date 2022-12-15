5f506f765547a9bd33c51c7185f7bb378bd8625f
Motti Dadison <motti.dadison@gmail.com>
2022-12-15 20:01:41 +0200

    Update aws-vault-terraform.sh

--- scripts/aws-vault-terraform.sh
+++ scripts/aws-vault-terraform.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 ### NOTES:
-### This is a wrapper for AWS-VAULT to use Terraform using MFA 
+### to run this file in the terminal: ./terraform.sh <arg1> <arg2>
 ### see help for more info: ./terraform.sh -h
-### aws-vault utilise the aws config file, so make sure you have the right profile in place.
+### aws-vault is using the aws config file and not the credentials file
 ### The source profile is set in the ~/.aws/config file
 ### in your config file you need to set the following:
-###
-### [profile my-example]
+### [profile niio-example]
 ### region = us-east-1
 ### mfa_serial = arn:aws:iam::123456789012:mfa/user
 ### role_arn = arn:aws:iam::123456789012:role/example
@@ -21,13 +20,38 @@ yellow='\033[0;33m'
 cyan='\033[0;36m'
 reset='\033[0m'
 
+# Vault port-forwarding to avoid the need to run it manually each time
+vault_port=8200
+nc -zv localhost $vault_port 2>&1 # Checks if already listening
+if [ "$?" -ne 0 ];then
+  kubectl port-forward --namespace vault vault-0 $vault_port:$vault_port > /dev/null 2>&1 &
+  echo -e "${cyan}Vault is listening on port $vault_port ${reset}"
+else
+  echo -e "${cyan}Vault is already listening on port $vault_port ${reset}"
+fi
+
+# This env variable is used by terraform
+export VAULT_ADDR=http://localhost:$vault_port
 export KUBE_CONFIG_PATH=~/.kube/config
 
 aws_vault=$(command -v aws-vault)
+vault_exec=$(command -v vault)
 
 if [ -z "$aws_vault" ]; then
-  echo -e "aws-vault not found. Please install it first by running: ${cyan}brew install --cask aws-vault${reset}"
+  echo -e "aws_vault not found. Please install it first by running: ${cyan}brew install --cask aws-vault${reset}"
   exit 1
+elif [ -z "$vault_exec" ]; then
+  echo -e "vault not found. Please install it first by running: ${cyan}brew install vault${reset}"
+  exit 1
+fi
+
+# Checks if the user is already logged in
+vault token lookup > /dev/null 2>&1
+if [ "$?" -ne 0 ]; then
+# shellcheck disable=SC2034, used in vault login
+  read -rp "Please enter your Vault username: " vault_user
+  echo -e "Please enter your Vault password"
+  vault login -method=userpass username="$vault_user"
 fi
 
 function usage() {
@@ -35,7 +59,7 @@ function usage() {
     echo "Options:"
     echo "  -a            Initialize a new key to the vault - RUN THIS FIRST"
     echo "  -c            Run a terraform command (init, plan, apply, all, destroy) - Mandatory"
-    echo "  -e            Environment to use for the terraform config file [staging/prod] - Mandatory"
+    echo "  -e            Environment to use for the terraform config file [staging/prod/dev] - Default to dev"
     echo "  -h            Prints this help message"
     echo "  -p            The AWS profile to use from the config file - Mandatory"
     echo "  -t            The terraform target to apply - Optional"
@@ -68,12 +92,14 @@ done
 if [ -z "$profile" ]; then
   echo "Profile not specified. Please specify a profile with the -p flag."
   exit 1
-elif [ -z "$command" ] && [ ! $add ]; then
+elif [ -z "$command" ]; then
+  if ! $add ; then
     echo "Command not specified. Please specify a command with the -c flag."
     exit 1
+  fi
 elif [ -z "$environment" ]; then
-  echo "Environment not specified. Using ${cyan}STAGING${reset} as default"
-  environment="staging"
+  echo -e "Environment not specified. Using ${cyan}STAGING${reset} as default"
+  environment="dev"
 fi
 
 function add() {
@@ -86,7 +112,7 @@ function add() {
 
 function init() {
     echo -e "${cyan}Initializing terraform...${reset}"
-    aws-vault exec "$profile" -- terraform init -backend-config="$environment.conf"
+    aws-vault exec "$profile" -- terraform init -reconfigure -backend-config="$environment.conf"
 }
 
 function plan() {
@@ -114,10 +140,10 @@ function destroy() {
 
 add
 case "$command" in
-    init) init "$@"       ;;
-    plan) plan "$@"       ;;
-    apply) apply "$@"     ;;
-    all) all              ;;
-    destroy) destroy "$@" ;;
-    *) exit 1             ;;
+    init) init        ;;
+    plan) plan        ;;
+    apply) apply      ;;
+    all) all          ;;
+    destroy) destroy  ;;
+    *) exit 1         ;;
 esac
